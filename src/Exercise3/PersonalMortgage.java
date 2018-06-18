430f959d56b4ebaeb50b0a65fc0028a6e6f9c291
Kashishrao <kashishrao40@gmail.com>
2018-06-17 22:39:23 -0400

    personal mortgage class created

---
+++ src/Exercise3/PersonalMortgage.java
@@ -0,0 +1,7 @@
+package Exercise3;
+
+public class PersonalMortgage extends Mortgage {
+    public PersonalMortgage(String bankName,int mortgageNumber, String customerName, double amountOfMortgage, int term) {
+        super(bankName,mortgageNumber, customerName, amountOfMortgage, 0.02, term);
+    }
+}
