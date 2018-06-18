e59949515474e344610d2faececf100d3eef9a9d
Kashishrao <kashishrao40@gmail.com>
2018-06-17 22:37:44 -0400

    mortgage constant class created

---
+++ src/Exercise3/MortgageConstants.java
@@ -0,0 +1,9 @@
+package Exercise3;
+
+public interface MortgageConstants {
+    String bankName = null;
+    int MaxAmount = 300_000;
+    int shortTerm = 1;
+    int mediumTerm = 3;
+    int longTerm = 5;
+}
